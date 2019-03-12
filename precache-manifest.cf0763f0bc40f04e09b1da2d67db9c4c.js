925a647a8ddb26be9a209cbae4e33366bbe692b5
defseg <6988161+defseg@users.noreply.github.com>
2019-03-12 06:59:51 -0400

    Add gh-pages build

---
+++ precache-manifest.cf0763f0bc40f04e09b1da2d67db9c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85d2c8956d7ee8e8128b",
+    "url": "/pshrimp-client/static/js/runtime~main.85d2c895.js"
+  },
+  {
+    "revision": "5c7d3422a0265d176df4",
+    "url": "/pshrimp-client/static/js/main.5c7d3422.chunk.js"
+  },
+  {
+    "revision": "4e8447b0feb436725e53",
+    "url": "/pshrimp-client/static/js/2.4e8447b0.chunk.js"
+  },
+  {
+    "revision": "5c7d3422a0265d176df4",
+    "url": "/pshrimp-client/static/css/main.374f87f7.chunk.css"
+  },
+  {
+    "revision": "4e8447b0feb436725e53",
+    "url": "/pshrimp-client/static/css/2.ad07282f.chunk.css"
+  },
+  {
+    "revision": "aae54e67ed1b800b584331658745369e",
+    "url": "/pshrimp-client/index.html"
+  }
+];
