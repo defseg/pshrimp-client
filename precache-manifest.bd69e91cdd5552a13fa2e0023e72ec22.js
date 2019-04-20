9aaf9b1e4c3a94e225cbdf7af374edf0e44c08cc
defseg <6988161+defseg@users.noreply.github.com>
2019-04-20 14:07:45 -0400

    2.0.0 but actually (fixed help text)

---
+++ precache-manifest.bd69e91cdd5552a13fa2e0023e72ec22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85d2c8956d7ee8e8128b",
+    "url": "/pshrimp-client/static/js/runtime~main.85d2c895.js"
+  },
+  {
+    "revision": "cc8f6d2a01a467c9582a",
+    "url": "/pshrimp-client/static/js/main.cc8f6d2a.chunk.js"
+  },
+  {
+    "revision": "5dac9c3b318b6dfc983c",
+    "url": "/pshrimp-client/static/js/2.5dac9c3b.chunk.js"
+  },
+  {
+    "revision": "cc8f6d2a01a467c9582a",
+    "url": "/pshrimp-client/static/css/main.6fa6c118.chunk.css"
+  },
+  {
+    "revision": "5dac9c3b318b6dfc983c",
+    "url": "/pshrimp-client/static/css/2.fb123e54.chunk.css"
+  },
+  {
+    "revision": "4731f0fc67a6bbf00c06bdfa39d71882",
+    "url": "/pshrimp-client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pshrimp-client/precache-manifest.1ff1edf4329316846de864cd2c5a8d49.js"
+  "/pshrimp-client/precache-manifest.bd69e91cdd5552a13fa2e0023e72ec22.js"
 );
 
 workbox.clientsClaim();
